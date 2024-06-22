70ea6047556f97441021881ea48239a10522ddf5
Sahiru <sahiru906@gmail.com>
2024-06-22 17:18:20 +0530

    updated

--- insert.php
+++ insert.php
@@ -4,23 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic PHP Form</title>
-    <style>
-        #hcbdjsh {
-            background-color: yellow;
-        }
-        #sjhjdaj{
-            background-color: red;
-        }
-        
-    </style>
-    
-    
 </head>
 <body>
     <form action="welcome.php" method="post">
         <label for="name">Name:</label>
         <input type="text" id="hcbdjsh" name="Name" required><br><br>
         
+        <label for="email">Email:</label>
+        <input type="email" id="sjhjdaj" name="Email" required><br><br>
         <label for="email">Email:</label>
         <input type="email" id="sjhjdaj" name="Email" required><br><br>
         Gender:
