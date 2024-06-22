ccf685dd0ae8afba7913a1da0c8566fd9f03288e
Sahiru <sahiru906@gmail.com>
2024-06-22 17:31:01 +0530

    first commit

--- insert.php
+++ insert.php
@@ -9,7 +9,8 @@
     <form action="welcome.php" method="post">
         <label for="name">Name:</label>
         <input type="text" id="hcbdjsh" name="Name" required><br><br>
-        
+        <label for="name">Password</label>
+        <input type="password" id="hcbdjsh" name="password" required><br><br>
         <label for="email">Email:</label>
         <input type="email" id="sjhjdaj" name="Email" required><br><br>
         <label for="email">Email:</label>
