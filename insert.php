78590819278c396260b722ce759762d208e3cd5c
Sahiru <sahiru906@gmail.com>
2024-06-29 12:14:10 +0530

    updated

--- insert.php
+++ insert.php
@@ -14,7 +14,7 @@
         <label for="email">Email:</label>
         <input type="email" id="sjhjdaj" name="Email" required><br><br>
         <label for="email">Email:</label>
-        <input type="email" id="sjhjdaj" name="Email" required><br><br>
+        <input type="number" id="sjhjdaj" name="Email" required><br><br>
         Gender:
 
 
